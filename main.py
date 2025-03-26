aa18ecd27c2e4d4616b66eadb31ab3ada513ce05
Fabio_Henrique <fo9660882@gmail.com>
2025-03-26 20:55:21 -0300

    soma teste

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 print("Bem vindo(a)!")
 
+
+def soma():
+    num01soma = float(input("Digite um numero:"))
+    num02soma = float(input("Digite um numero:"))
+    resultado_soma = num01soma + num02soma
+    print(f"Sua soma entre {num01soma} e {num02soma} é {resultado_soma}")
+
 def menu():
     while True:
         escolha_usuario = int(input("1 - Soma 2 - Subtração 3 - Multiplicacao 4 - Divisão "))
         if escolha_usuario == 1:
-            print("soma")
+            soma()
         elif escolha_usuario == 2:
             print("Subtração")
         elif escolha_usuario == 3:
