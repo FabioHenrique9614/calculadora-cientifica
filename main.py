9c02f7979777777faffb6dd5073932a38d8e0791
Fabio_Henrique <fo9660882@gmail.com>
2025-03-25 21:08:11 -0300

    teste menu

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 print("Bem vindo(a)!")
 
-# selecao de operacao(MENU)
-escolha_usuario = int(input("1 - Soma 2 - Subtração 3 - Multiplicacao 4 - Divisão"))
+def menu():
+    while True:
+        escolha_usuario = int(input("1 - Soma 2 - Subtração 3 - Multiplicacao 4 - Divisão "))
+        if escolha_usuario == 1:
+            print("soma")
+        elif escolha_usuario == 2:
+            print("Subtração")
+        elif escolha_usuario == 3:
+            print("Multiplicação")
+        elif escolha_usuario == 4:
+            print("Divisão")
+        else:
+            continue
+menu()
