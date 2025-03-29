735459f782b98903d8bd334748ce6db8b8b94d90
Fabio_Henrique <fo9660882@gmail.com>
2025-03-29 14:15:26 -0300

    bhaskara

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
-import math as m # importacao de biblioteca math
-print("Bem vindo(a)!")
+import math # importacao de biblioteca math
 
-
-def soma():
-    num01soma = float(input("Digite um numero:"))
-    num02soma = float(input("Digite um numero:"))
-    resultado_soma = num01soma + num02soma
-    print(f"Sua soma entre {num01soma} e {num02soma} é {resultado_soma}")
-
-def menu():
-    while True:
-        escolha_usuario = int(input("1 - Soma 2 - Subtração 3 - Multiplicacao 4 - Divisão "))
-        if escolha_usuario == 1:
-            soma()
-        elif escolha_usuario == 2:
-            print("Subtração")
-        elif escolha_usuario == 3:
-            print("Multiplicação")
-        elif escolha_usuario == 4:
-            print("Divisão")
-        else:
-            continue
-menu()
-
-print("teste")
-print("mateus Logado")
+def bhaskara(a, b, c):
+    delta = b**2 - 4*a*c  # Calcula o delta
+    
+    if delta < 0:
+        return "Não há raízes reais"  # Caso o delta seja negativo
+    
+    x1 = (-b + math.sqrt(delta)) / (2 * a)  # Primeira raiz
+    x2 = (-b - math.sqrt(delta)) / (2 * a)  # Segunda raiz
+    
+    return x1, x2  # Retorna as raízes"
