7eb185bdcb57833fa498f24971bba8548488ba39
FabioHenrique9614 <fabio.oliveira9614@gmail.com>
2025-04-02 07:20:37 -0300

    Bhaskara

--- main.py
+++ main.py
@@ -22,8 +22,35 @@ def divisao():
 
 #FABIO
 def bhaskara():
-    #codigo aqui
-    ...
+    while True:
+        try:
+            a = float(input("Digite o valor de a"))
+            if a == 0:
+                print('"a" não pode ser 0. Tente novamente')
+                continue
+            b = float(input("Digite o valor de b"))
+            c = float(input("Digite o valor de c"))
+        except ValueError:
+            print("Erro: Digite apenas números. Tente novamente.")
+            continue
+
+        delta = (b**2) -4 * a * c
+        print(f"Valor de DELTA é {delta}")
+
+        if delta >= 0:
+            break
+        else:
+            print(f"Delta negativo. Não há raizes reais. Tente outros coeficientes.\n")
+    
+    if delta > 0:
+        x1 = (-b + math.sqrt(delta)) / (2 * a)
+        x2 = (-b - math.sqrt(delta)) / (2 * a)
+        print(f"Duas raizes reais: X1 = {x1:.2f}, X2 = {x2:.2f}")
+    else:
+        x = -b / (2 * a)
+        print(f"Raiz dupla: x = {x:.2f}")
+
+bhaskara()
 
 #MATEUS
 def expressões_algébricas():
